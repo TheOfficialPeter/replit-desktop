339ae7b8878f7a949a9d7f8d4c8c103af42bc867
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2022-07-27 16:20:41 +0200

    final

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
+function initWindow() {
+	var win = new BrowserWindow({width: 800, height: 600});
+
+	win.loadURL("https://replit.com/~");
+	win.once('ready-to-show', () => {
+  		win.show();
+	});
+	win.on('closed', function() {
+		win = null;
+	});
+};
+
+app.whenReady().then(() =>{
+	initWindow();
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+* a replit-desktop clone *
+** Could not seem to find one on github so I quickly made my own **
